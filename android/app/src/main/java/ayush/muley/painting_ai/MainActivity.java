f2038f55bc1e78634881f23104185e289b88b120
Ayush2006128 <ayush.muley1907@outlook.com>
2025-05-23 05:41:21 +0000

    Added platforms: Android

---
+++ android/app/src/main/java/ayush/muley/painting_ai/MainActivity.java
@@ -0,0 +1,5 @@
+package ayush.muley.painting_ai;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ayush.muley.painting-ai',
+  appId: 'ayush.muley.painting_ai',
   appName: 'painting-ai',
   webDir: 'dist'
 };
